9d0902ec9c06e68380d5698d741b655d73c630b1
Fernando Cruz <eufernandoferreirazz@gmail.com>
2025-04-14 20:07:02 -0300

    Update Salario.java

--- Exercicios/Salario.java
+++ Exercicios/Salario.java
@@ -11,7 +11,6 @@ static double salario[] = new double[n];
 static int[] idade = new int[n];
 static String[] nomes = new String[n]; 
 
-    
 public static void novaLinha(Scanner leitor) {
     linha += 1;
 
@@ -25,34 +24,39 @@ public static void novaLinha(Scanner leitor) {
 
     System.out.print("Idade: ");
     idade[linha] = leitor.nextInt();     
-    }
+}
 
 public static void main(String args[]) {
 
-    Scanner leitor = new Scanner(System.in);
-    double aux, itemMenu;
+Scanner leitor = new Scanner(System.in);
+double novoSalario, itemMenu;
 
     while (true) {
         System.out.print("\n1- Ler Dados\n2- Mostrar Dados\n3- Sair\nOpção: ");
         itemMenu = leitor.nextInt();
 
-     if (itemMenu == 1) {
-                novaLinha(leitor);
-            } else if (itemMenu == 2) {
-                System.out.println("\nNome           Salário (R$)   Aumento (R$)   Idade");
+        if (itemMenu == 1) {
+            novaLinha(leitor);
+        } else if (itemMenu == 2) {
+            System.out.println("\nNome           Salário (R$)   Novo Salário (R$)   Idade");
 
-                for (int i = 0; i <= linha; i++) {
-                    aux = salario[i] * 1.1; 
-                    System.out.printf("%-15s %-15.2f %-15.2f %-10d\n", 
-                        nomes[i], salario[i], aux, idade[i]);
-                }
-            } else if (itemMenu == 3) {
-                break;
+        for (int i = 0; i <= linha; i++) {
+                  
+            if (idade[i] > 18) {
+                novoSalario = salario[i] * 1.15; 
             } else {
-                System.out.println("\nOpção inválida. Tente novamente.");
+                novoSalario = salario[i] * 1.08; 
             }
+                    
+            System.out.printf("%-15s %-15.2f %-15.2f %-10d\n", 
+                nomes[i], salario[i], novoSalario, idade[i]);
         }
-
-        System.exit(0);  // Encerra o programa
+    } else if (itemMenu == 3) {
+        break;
+    } else {
+        System.out.println("\nOpção inválida. Tente novamente.");
     }
+  }
+    System.exit(0);  // Encerra o programa
+ }
 }
